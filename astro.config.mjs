996a2f5e27711797723a1d461525813be90968cf
guygubaby <guygubaby@gmail.com>
2024-01-17 11:25:10 +0800

    feat: update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
+import process from 'node:process'
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 import node from '@astrojs/node'
-import vercel from '@astrojs/vercel/edge'
+import vercel from '@astrojs/vercel/serverless'
 import AutoImport from 'unplugin-auto-import/astro'
 import Vue from '@astrojs/vue'
 
